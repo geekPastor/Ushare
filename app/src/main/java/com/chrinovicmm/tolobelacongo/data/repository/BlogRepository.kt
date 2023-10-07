26607aa9594ddb4a7a946cbc73621e3236b03890
chrinovic mm <chrinovicmukeba123@gmail.com>
2023-10-07 14:40:57 +0200

    add create user

--- app/src/main/java/com/chrinovicmm/tolobelacongo/data/repository/BlogRepository.kt
+++ app/src/main/java/com/chrinovicmm/tolobelacongo/data/repository/BlogRepository.kt
@@ -1,5 +1,31 @@
 package com.chrinovicmm.tolobelacongo.data.repository
 
-class BlogRepository {
+import com.chrinovicmm.tolobelacongo.domain.model.User
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.chrinovicmm.tolobelacongo.util.Result
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.tasks.await
 
+class BlogRepository {
+    suspend fun signOut(oneTapClient: SignInClient) = flow {
+        emit(Result.Loading)
+        oneTapClient.signOut().await()
+        Firebase.auth.signOut()
+        emit(Result.Success(true))
+    }.catch { error->
+        emit(Result.Error(error))
+    }
+
+    fun getSignedUser() = flow {
+        emit(Result.Loading)
+        val firebaseCreateUser = Firebase.auth.currentUser
+        val user = if (firebaseCreateUser != null){
+            User(firebaseCreateUser.uid, firebaseCreateUser.displayName, firebaseCreateUser.photoUrl.toString())
+        }else null
+
+        emit(Result.Success(user))
+    }
 }
