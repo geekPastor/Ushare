37fc73f6511aa1e59635509d04c9262bd31606d1
chrinovic mm <chrinovicmukeba123@gmail.com>
2023-10-07 15:14:26 +0200

    add application ui providers

--- app/src/main/java/com/chrinovicmm/tolobelacongo/dependanciesIncluded/AppModule.kt
+++ app/src/main/java/com/chrinovicmm/tolobelacongo/dependanciesIncluded/AppModule.kt
@@ -1,4 +1,29 @@
 package com.chrinovicmm.tolobelacongo.dependanciesIncluded
 
+import android.content.Context
+import com.chrinovicmm.tolobelacongo.util.GoogleAuthUiHelper
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+    @Provides
+    fun provedesSignedClient(@ApplicationContext context: Context): SignInClient{
+        return Identity.getSignInClient(context)
+    }
+
+    @Provides
+    fun providesGoogleAuthSignInClient(
+        signInClient: SignInClient,
+        @ApplicationContext context: Context
+    ): GoogleAuthUiHelper{
+        return GoogleAuthUiHelper(context, signInClient)
+    }
 }
