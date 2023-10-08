b1b7848ec6ff55c75ccf801568915dbb578b8795
chrinovic mm <chrinovicmukeba123@gmail.com>
2023-10-08 07:26:49 +0200

    Add login Screen

--- app/src/main/java/com/chrinovicmm/tolobelacongo/ui/MainActivity.kt
+++ app/src/main/java/com/chrinovicmm/tolobelacongo/ui/MainActivity.kt
@@ -10,34 +10,46 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.chrinovicmm.tolobelacongo.ui.theme.TolobelaCongoTheme
+import com.chrinovicmm.tolobelacongo.util.GoogleAuthUiHelper
+import com.google.android.gms.auth.api.identity.SignInClient
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject lateinit var googleAuthUiHelper: GoogleAuthUiHelper
+    @Inject lateinit var oneTapClient : SignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TolobelaCongoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+
+                    val viewModel : MainViewModel = hiltViewModel()
+                    val uiState = viewModel.uiState.value
+                    
+                    NavHost(navController = navController, startDestination =  "signin"){
+                        composable("signin"){
+
+                        }
+
+                        composable("home"){
+
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TolobelaCongoTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/chrinovicmm/tolobelacongo/ui/screen/SignInScreen.kt
@@ -0,0 +1,53 @@
+package com.chrinovicmm.tolobelacongo.ui.screen
+
+import android.graphics.Paint.Align
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.chrinovicmm.tolobelacongo.domain.model.User
+
+@Composable
+fun SignInScreen(
+    isLoading: Boolean,
+    currentUser: User,
+    onSignInClick: ()->Unit
+){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Star,
+                contentDescription = null,
+                modifier = Modifier.size(180.dp)
+            )
+            if (isLoading || currentUser == null){
+                Button(onClick = onSignInClick) {
+                    Text(
+                        text = "Se connecter"
+                    )
+                }
+            }
+        }
+    }
+
+}
