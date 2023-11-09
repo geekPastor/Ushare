60a15f09008a86adb71b5e230213499b42c2c67a
chrinovic mm <chrinovicmukeba123@gmail.com>
2023-11-09 22:00:27 +0100

    feat Blog Update screen

--- app/src/main/java/com/chrinovicmm/tolobelacongo/ui/screen/UpdateBlogScreen.kt
+++ app/src/main/java/com/chrinovicmm/tolobelacongo/ui/screen/UpdateBlogScreen.kt
@@ -1,2 +1,23 @@
 package com.chrinovicmm.tolobelacongo.ui.screen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.chrinovicmm.tolobelacongo.ui.theme.TolobelaCongoTheme
+
+@Composable
+fun UpdateBlogScreen(
+    blogTitle: String?,
+    blogContent: String?,
+    thumbnail: String?
+){
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UpdateBlogScreenPreview(){
+    TolobelaCongoTheme {
+        UpdateBlogScreen("", "", "")
+    }
+}
