88dfe951b050ef89cfcfd02cadd10db598d1bd18
chrinovic mm <chrinovicmukeba123@gmail.com>
2023-11-10 10:29:34 +0100

    start working on update blog page

--- app/src/main/java/com/chrinovicmm/tolobelacongo/ui/screen/UpdateBlogScreen.kt
+++ app/src/main/java/com/chrinovicmm/tolobelacongo/ui/screen/UpdateBlogScreen.kt
@@ -1,23 +1,76 @@
 package com.chrinovicmm.tolobelacongo.ui.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ModifierInfo
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.chrinovicmm.tolobelacongo.R
 import com.chrinovicmm.tolobelacongo.ui.theme.TolobelaCongoTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UpdateBlogScreen(
     blogTitle: String?,
     blogContent: String?,
-    thumbnail: String?
+    thumbnail: String?,
+    onBackPressed: ()->Unit
 ){
+    
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+        var title by remember {
+            mutableStateOf(blogTitle ?: "")
+        }
+
+        var selectedThumbnail by remember {
+            mutableStateOf(thumbnail ?: "")
+        }
+
+        var content by remember {
+            mutableStateOf(blogContent ?: "")
+        }
 
 
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun UpdateBlogScreenPreview(){
     TolobelaCongoTheme {
-        UpdateBlogScreen("", "", "")
+        UpdateBlogScreen("", "", "", {})
     }
 }
